cfff6be7f5a9c4b3b3ff443fa4f382063132c6ab
Tysiachnyk <vtysiachnyk@gmail.com>
2026-04-15 09:53:41 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let amountAdded = 0;
 
   return function adder(num) {
@@ -21,4 +21,4 @@ function makeInfinityAdder() {
   };
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
